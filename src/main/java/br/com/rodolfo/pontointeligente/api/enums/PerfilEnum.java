e7d45cb08296f3de7d590dff12ef67ce516c9cdf
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2018-07-08 15:30:53 -0300

    Criação das Entidades e Enums para a Aplicação

---
+++ src/main/java/br/com/rodolfo/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,12 @@
+package br.com.rodolfo.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+    
+    INICIO_TRABALHO,
+    TERMINO_TRABALHO,
+    INICIO_ALMOCO,
+    TERMINO_ALMOCO,
+    INICIO_PAUSA,
+    TERMINO_PAUSA;
+
+}
---
+++ src/main/java/br/com/rodolfo/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,8 @@
+package br.com.rodolfo.pontointeligente.api.enums;
+
+public enum TipoEnum {
+
+    ROLE_ADMIN,
+    ROLE_USUARIO;
+
+}
